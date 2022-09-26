b3420490c66711d53325a3df08079c2f6e5b245a
huseyin <hsyn_hsyn77@hotmail.com>
2022-09-26 13:19:16 +0300

    Initial commit

---
+++ src/Proje_20.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Proje_20 {
+    public static void main(String[] args) {
+        int n, k;
+        int total=1;
+        Scanner input=new Scanner(System.in);
+        System.out.print("üssü alınacak sayı  : ");
+        n=input.nextInt();
+        System.out.print("üs olacak sayı  :");
+        k=input.nextInt();
+
+        for (int i = 0; i < k; i++) {
+            total*=n;
+
+        }
+        System.out.println(n+ " üssü"+"="+total);
+
+    }
+}
